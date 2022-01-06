f0a03a1efbe76fdf33dc76bc91dc87de9102d2bd
Matias Eguez <matias.eguez@warnermedia.com>
2022-01-05 21:05:06 -0300

    Added html files and bootstrap

---
+++ AppCoder/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from AppCoder.models import Curso
+from AppCoder import views
+
+
+urlpatterns = [
+    path('', views.inicio),
+    path('curso/', views.curso),
+    path('profesores/', views.profesores),
+    path('estudiantes/', views.estudiantes),
+    path('entregables/', views.entregables)
+]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -11,3 +11,23 @@ def crea_curso(self, nombre, camada):
     curso.save()
 
     return HttpResponse(f'Se creo el curso de {curso.nombre} con el numero de camada: {curso.camada}')
+
+def inicio(request):
+
+    return render(request, 'AppCoder/inicio.html')   
+
+def curso(request):
+
+    return render(request, 'AppCoder/cursos.html') 
+
+def profesores(request):
+
+    return render(request, 'AppCoder/profesores.html') 
+
+def estudiantes(request):
+
+    return render(request, 'AppCoder/estudiantes.html') 
+
+def entregables(request):
+
+    return render(request, 'AppCoder/entregables.html') 
